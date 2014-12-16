251094379d9b8868a239f9a6c1fb7f517fa8ab7e
Yann Chemin <ychemin@gmail.com>
2014-12-16 09:38:10 +0530

    Updated the building script

--- build.sh
+++ build.sh
@@ -11,4 +11,22 @@ cd src.sspp
 make
 sudo make install
 
+#copy utilities 
+cd utility
+sudo cp check_env /usr/local/bin
+sudo cp specpr    /usr/local/bin
+sudo cp sp_spool  /usr/local/bin
+sudo cp specprhelp /usr/local/bin
+sudo cp specprnt   /usr/local/bin
+sudo cp spedit     /usr/local/bin
+sudo cp spfind     /usr/local/bin
+
+cd /usr/local/bin/
+sudo ln specpr dspecpr
+sudo ln specpr radtran
+sudo ln specpr dradtran
+
+
+
+
 
