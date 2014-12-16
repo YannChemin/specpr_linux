6a181d0aa0d73c49050cfa41694c8b3ddfac7352
Yann Chemin <ychemin@gmail.com>
2014-12-16 10:31:21 +0530

    Update

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash 
 
+#we are at the specpr_linux/ root
+DIR=$(pwd)
+
 #Basic dependencies
 sudo apt-get install byacc flex gfortran -y
 
 #Please make sure you installed ratfor through apt-get / yum / etc.
 sudo apt-get install ratfor -y
 
-#-----------------------
 #Build required software
-#-----------------------
-
-cd src.sspp
+cd $DIR/src.sspp
 make
 sudo make install
 
 #copy utilities 
-cd utility
+cd $DIR/utility
 sudo cp check_env /usr/local/bin
 sudo cp specpr    /usr/local/bin
 sudo cp sp_spool  /usr/local/bin
@@ -31,11 +31,14 @@ sudo ln specpr radtran
 sudo ln specpr dradtran
 
 #Compile specpr
-cd /usr/local/src/specpr/config
+#sudo su
+cd $DIR/config
 source ./cshenv.linux64.gfortran
-cd /usr/local/src/specpr/src.specpr
+cd $DIR/src.specpr
 make really_clean
 make
+#exit
+
 
 
 
